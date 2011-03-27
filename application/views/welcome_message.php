97db5eaca3279e1bd6c7f3802bf80938ed2fdb69
Chris <cleblanc87@gmail.com>
2011-03-26 22:09:43 -0700

    worthless change!

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ require_once '/home/chris/public_html/cleblanc/haml/HamlHelpers.php';
 <link href="../stylesheets/screen.css" rel="stylesheet" type="text/css" /></head><div>
 <?php echo $ci_msg_str; ?>
 
-<br />aml haml haml
+<br />hhhhhhaml haml haml
 <p class="lol">
 this be some haml son
 </p></div><canvas>
