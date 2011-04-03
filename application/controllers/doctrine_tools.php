75f1cc704bbdf7a2de2eeb16fb2e719e35fe7498
Chris <cleblanc87@gmail.com>
2011-04-03 15:20:57 -0600

    sdfsdf

--- application/controllers/doctrine_tools.php
+++ application/controllers/doctrine_tools.php
@@ -1,16 +1,23 @@
 <?php
 class Doctrine_Tools extends CI_Controller {
+  function create_tables() {
+    echo 'Reminder: Make sure the tables do not exist already.<br />
+          <form action="" method="POST">
+          <input type="submit" name="action" value="Create Tables"><br /><br />';
+    if ($this->input->post('action')) {
+      Doctrine::createTablesFromModels();
+      echo "Done!";
+    }
+  }
+  function run_migrations() {
+    echo 'Reminder: Migrate Time.<br />
+          <form action="" method="POST">
+          <input type="submit" name="action" value="Run Migration"><br /><br />';
+    if ($this->input->post('action')) {
+      $migration = new Doctrine_Migration('../migrations');
+      $migration->migrate();
+    }
 
-	function create_tables() {
-		echo 'Reminder: Make sure the tables do not exist already.<br />
-		<form action="" method="POST">
-		<input type="submit" name="action" value="Create Tables"><br /><br />';
-
-		if ($this->input->post('action')) {
-			Doctrine::createTablesFromModels();
-			echo "Done!";
-		}
-	}
-
+  }
 }
 ?>
